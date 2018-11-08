94c3d285d385fa92d4fcdd401b97cf1e61d9861e
yuriy.tsapenko <yuriy.tsapenko.55@gmail.com>
2018-11-08 17:15:53 +0200

    Made buttons interactive

--- app/src/main/java/com/android/ytsapenko/lesson2task1/MainActivity.kt
+++ app/src/main/java/com/android/ytsapenko/lesson2task1/MainActivity.kt
@@ -2,11 +2,43 @@ package com.android.ytsapenko.lesson2task1
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toastButton:Button = findViewById<Button>(R.id.toast_button)
+        toastButton.setOnClickListener({
+            Toast.makeText(this,"Hello toast!",Toast.LENGTH_SHORT).show()
+        })
+        val randomButton:Button = findViewById<Button>(R.id.random_button)
+        count_button.setOnClickListener({
+            countMe(count_button)
+        })
+        val countButton:Button = findViewById(R.id.count_button)
+        countButton.setOnClickListener({
+
+        })
+    }
+
+    fun countMe(view: View){
+        val countTextView: TextView = findViewById(R.id.textView)
+        val countString:String = countTextView.text.toString()
+        var count:Int = Integer.parseInt(countString)
+        count++;
+        countTextView.text = count.toString()
+    }
+
+    fun randomMe(view: View){
+        val countTextView: TextView = findViewById(R.id.textView)
+        countTextView.text = Random(10).toString()
     }
 }
